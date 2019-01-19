d738fcca696197128ea58551b87ce4795faa4756
dave <dub.cyclone@gmail.com>
2019-01-19 22:04:52 +0000

    Add string to vector3 extension for the client, X: Y: Z:.

--- src/GTAN.Extensions/Vector3Extensions.cs
+++ src/GTAN.Extensions/Vector3Extensions.cs
@@ -25,5 +25,24 @@ namespace GTAN.Extensions
                     float.Parse(v3s[1]),
                     float.Parse(v3s[2]));
         }
+
+        /// <summary>
+        /// Converts a Vector3 string to <see cref="RAGE.Vector3"/>. "X:0 Y:0 Z:0.5";
+        /// </summary>
+        /// <param name="vector3"></param>
+        /// <param name="vector3String"></param>
+        /// <returns></returns>
+        public static GTANetworkAPI.Vector3 ConvertClientVector3(this Vector3 vector3, string vector3String)
+        {
+            if (string.IsNullOrEmpty(vector3String)) return new GTANetworkAPI.Vector3();
+
+            string vStr = vector3String
+                .Replace("X:", string.Empty)
+                .Replace(" Y:", string.Empty)
+                .Replace(" Z:", string.Empty);            
+
+            return
+                ConvertToVector3(vector3, vStr);
+        }
     }
 }
